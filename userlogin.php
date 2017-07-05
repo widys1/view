640086a31c15fbb44efe4d6933266ff09c468361
fikaamalia <vkfiku@gmail.com>
2017-07-05 13:53:10 +0700

    Add files via upload

---
+++ userlogin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Login User</title>
+  <script src="<?php echo base_url()?>assets/login/js/index.js" type="text/javascript"></script>
+
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  
+      <link rel="stylesheet" href="<?php echo base_url()?>/assets/login/css/style.css">
+
+  
+</head>
+
+<body>
+  <div class="login">
+    <header class="header">
+      <span class="text">LOGIN</span>
+      <span class="loader"></span>
+   </header>
+  <form class="form">  
+    <input class="input" type="text", placeholder="Username">
+    <input class="input" type="password" placeholder="Password">
+    <button class="btn" type="submit"></button>
+  </form>
+</div>
+<button class ="resetbtn" type= "reset">Reset it
+</button>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="<?php echo base_url()?>assets/login/js/index.js"></script>
+
+</body>
+</html>
